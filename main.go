192613ab7c2673f75bb104da543280eb09488e47
unknown <andreevivan8891@gmail.com>
2023-12-25 13:57:37 +0300

    some fixes

--- main.go
+++ main.go
@@ -22,6 +22,15 @@ func InitRouter() {
 
 func HandleAlice(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Query())
-	encodeValues := r.URL.Query().Encode()
-	fmt.Println("encodeValues:>", encodeValues)
+	client_id := r.URL.Query().Get("client_id")
+	redirect_uri := r.URL.Query().Get("redirect_uri")
+	response_type := r.URL.Query().Get("response_type")
+	state := r.URL.Query().Get("state")
+	fmt.Println("client_id:>", client_id)
+	fmt.Println("redirect_uri:>", redirect_uri)
+	fmt.Println("response_type:>", response_type)
+	fmt.Println("state:>", state)
+
+	//encodeValues := r.URL.Query().Encode()
+	//fmt.Println("encodeValues:>", encodeValues)
 }
