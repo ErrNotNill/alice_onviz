bfe120c8c49712992fed67106a25177c9716d8fc
unknown <andreevivan8891@gmail.com>
2023-12-29 11:00:24 +0300

    some fixes

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func ReadEmailFromLoginPageAndRedirect(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("reqId:>", reqId)
 
 	http.Redirect(w, r, "https://oauth.yandex.ru/authorize?response_type=code&client_id=4fed8408c435482b950afeb2d6e0f3cc&redirect_uri=https://social.yandex.net/broker/redirect", http.StatusFound)
-
+	fmt.Println("redirect ended:...")
 	body := []byte(``)
 	fmt.Println("code:>", r.URL.Query().Get("code"))
 	//code := r.URL.Query().Get("code")
